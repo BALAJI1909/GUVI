2220bcb1b4577e74a8190f9d83737de2bfd5f203
BALAJI1909 <47314301+BALAJI1909@users.noreply.github.com>
2019-08-07 15:46:04 +0530

    Add files via upload

---
+++ guvi2.py
@@ -0,0 +1,8 @@
+a=int(input())
+        printf("even");
+    if a/2==0:
+        printf("add");
+         elif a/2==0:
+             print("invalid")
+             else:
+                 print("add")
